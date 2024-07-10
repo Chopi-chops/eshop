047d75bd0edd0d82df52d07bc0a16b25a980824e
Chopi-chops <sultanbekovacholpon@gmail.com>
2024-07-11 01:54:09 +0600

    add costumers

--- costumerapp/views.py
+++ costumerapp/views.py
@@ -4,5 +4,5 @@ from .models import Costumer
 # Create your views here.
 def costumer_view(request):
     costumer_list = Costumer.objects.all()
-    context = {"costumer": costumer_list}
-    return render(request, 'index.html', context)
+    context = {"costumers": costumer_list}
+    return render(request, 'costumers.html', context)
