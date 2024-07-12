4ef77f9cfcc1b21a33d95cc9264c1a6a7e025834
Chopi-chops <sultanbekovacholpon@gmail.com>
2024-07-12 09:48:55 +0600

    domashka

--- core/models.py
+++ core/models.py
@@ -1,9 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+from costumerapp.models import Costumer
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(verbose_name="Описание", null=True, blank=False)
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     price = models.IntegerField()
     qty = models.IntegerField(default=0)
+    category = models.ForeignKey(
+        to=Category,
+        on_delete=models.SET_NULL,
+        null=True, blank=True,
+        verbose_name="Категория",
+    )
+    costumer_views = models.ManyToManyField(
+        to=Costumer,
+        blank=True
+    )
 
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
+    bio = models.TextField(null=True, blank=True)
+    social_link = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+    user = models.OneToOneField(
+        to=User,
+        on_delete=models.SET_NULL,
+        null=True, blank=True
+    )
--- news/models.py
+++ news/models.py
@@ -1,10 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class NewsCategory(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 class New(models.Model):
     title = models.CharField(max_length=1000)
     article = models.TextField(null=True, blank=True)
-    views = models.IntegerField()
+    views = models.IntegerField(default=0)
+    user_views = models.ManyToManyField(
+        to=User,
+        blank=True,
+    )
+    category = models.ForeignKey(
+        to=NewsCategory,
+        on_delete=models.SET_NULL,
+        null=True, blank=True,
+        verbose_name="Категория",
+    )
 
     def __str__(self):
         return self.title
+
+
+
